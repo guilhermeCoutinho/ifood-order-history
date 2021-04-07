898d5f36d00bf38bc81f0d01f678a9f5dc85b4bc
guilhermeCoutinho <guilhermejcoutinho@gmail.com>
2021-04-07 19:32:22 -0300

    Update: code works with updates to the API now, the way the authentication works is different now

---
+++ constants.go
@@ -0,0 +1,9 @@
+package main
+
+const (
+	historyLen        = 80
+	maxItensPerPage   = 20
+	wrongUsageMessage = "Usage: make run STARTING_DATE='dd-mm-yyyy' END_DATE='dd-mm-yyyy'"
+	timeFormat        = "02-01-2006" // dd-mm-yyyy
+	authToken         = ""
+)
